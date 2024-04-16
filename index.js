8b2faeeb14a49c78004b558d05e165c240bf97cc
Mausumi  Ghadei <117946464+Mausumi134@users.noreply.github.com>
2024-04-16 21:40:20 +0530

    updated backend

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+const port=process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`SERVER HAS STARTED AT PORT ${port}`);
 })
